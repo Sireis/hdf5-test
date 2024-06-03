f0f0b2d1080f66cffbf05e7b2891d1207e6b2084
Sireis <JensDieterUlrich@hotmail.de>
2024-06-03 23:14:06 +0200

    changed to a realistic job execution time maximum

--- job-hdf5-staging-evaluation.sh
+++ job-hdf5-staging-evaluation.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=00:15:00
+#SBATCH --time=1:00:00
 #SBATCH --partition=batch
 
 # *** start of job script ***
